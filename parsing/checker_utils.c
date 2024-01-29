cc22f5da9fa3cee6f1c9c5d5b662124f0cc6521a
Victor Libert <vilibert@shi-r8-p4.s19.be>
2024-01-29 11:24:40 +0100

    patch syntax error

--- parsing/checker_utils.c
+++ parsing/checker_utils.c
@@ -6,30 +6,11 @@
 /*   By: vilibert <vilibert@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 18:59:50 by vilibert          #+#    #+#             */
-/*   Updated: 2024/01/24 11:58:33 by vilibert         ###   ########.fr       */
+/*   Updated: 2024/01/29 11:22:51 by vilibert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
-static void	syntax_find(char *str, int *i, int *cmd)
-{
-	if (str[*i] == '<' || str[*i] == '>')
-	{
-		*cmd = 0;
-		if (str[*i] == '<')
-		{
-			if (str[*i + 1] == '<')
-				(*i)++;
-		}
-		if (str[*i] == '>')
-		{
-			if (str[*i + 1] == '>')
-				(*i)++;
-		}
-	}
-	(*i)++;
-}
 
 static void	syntax_quotes(char *str, int *i, int *cmd)
 {
@@ -54,12 +35,32 @@ static void	syntax_cmd(char *str, int *i, int *cmd)
 	while (str[*i] && str[*i] != '>' && str[*i] != '<')
 	{
 		syntax_quotes(str, i, cmd);
-		if (str[*i] == '|' && !cmd)
+		if (str[*i] == '|' && !(*cmd))
 			return ;
 		if (str[*i] != ' ')
 			*cmd = 1;
 		if (str[*i])
 			(*i)++;
+	}
+}
+
+static void	syntax_find(char *str, int *i, int *cmd)
+{
+	if (str[*i] == '<' || str[*i] == '>')
+	{
+		*cmd = 0;
+		if (str[*i] == '<')
+		{
+			if (str[*i + 1] == '<')
+				(*i)++;
+		}
+		if (str[*i] == '>')
+		{
+			if (str[*i + 1] == '>')
+				(*i)++;
+		}
+		(*i)++;
+		syntax_cmd(str, i, cmd);
 	}
 }
 
@@ -73,7 +74,6 @@ int	check_syntax_error(t_data *data, char *str)
 	while (str[i])
 	{
 		syntax_find(str, &i, &cmd);
-		syntax_cmd(str, &i, &cmd);
 		if (!cmd)
 			return (print_syntax_error(data, str[i]), 1);
 		if (str[i])
